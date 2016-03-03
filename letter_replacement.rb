3be2eeb02c28ffe36a58bc02351c210ba49771d8
agnik <reira@wp.pl>
2016-03-03 22:22:36 +0100

    Letter_replacement modified

--- letter_replacement.rb
+++ letter_replacement.rb
@@ -1,7 +1,7 @@
-print "Write"
+print "Write: "
 user_input = gets.chomp
 user_input.downcase!
 if user_input.include? "s"
    puts user_input.gsub!(/s/, "th")
-else print "Nothing to do here"
+else print "Nothing to replace here."
 end
